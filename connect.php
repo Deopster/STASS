d54b43c8edff5e93c6d6376e82ae899999be3e5f
Deopster <68296704+Deopster@users.noreply.github.com>
2021-12-04 20:05:01 +0300

    Initial commit

---
+++ connect.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'localhost'; //имя хоста, на локальном компьютере этоlocalhost
+$user = 'root'; //имя пользователя, по умолчанию это root
+$password = ''; //пароль, по умолчанию пустой
+$db_name = 'zachetka'; //имя базы данных
+
+$hostname = "localhost";
+$username = "root";
+$pass = "";
+$dbname = "zachetka";
+$table = "auth";
+$field_login="login";
+$field_password="password";
+?>
